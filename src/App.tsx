34fe7c2dc99a1f2e5feb38adc3cbd60a4648aede
Olena <nuchuporuk@gmail.com>
2020-01-30 21:12:24 +0100

    add map

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,25 @@
 import React from "react";
-import logo from "./logo.svg";
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
+import { withGoogleMap, GoogleMap, Marker } from "react-google-maps";
 
 const Wrapper = styled.div`
   text-align: center;
 `;
-
-const Header = styled.header`
-  background-color: #282c34;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
+const MapWrapper = styled.div`
+  width: auto;
+  height: 100vh;
 `;
 
-const animation = keyframes`
-from {
-  transform: rotate(0deg);
-}
-
-to {
-  transform: rotate(360deg);
-}`;
-
-const Img = styled.img`
-  height: 40vmin;
-  pointer-events: none;
-
-  @media (prefers-reduced-motion: no-preference) {
-    animation: ${animation} infinite 2s linear;
-  }
-`;
-
-const A = styled.a`
-  color: #61dafb;
-`;
+const Map = withGoogleMap(props => (
+  <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
+    <Marker position={{ lat: -34.397, lng: 150.644 }} />
+  </GoogleMap>
+));
 
 const App: React.FC = () => {
   return (
     <Wrapper>
-      <Header>
-        <Img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <A href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </A>
-      </Header>
+      <Map containerElement={<MapWrapper />} mapElement={<MapWrapper />} />
     </Wrapper>
   );
 };
