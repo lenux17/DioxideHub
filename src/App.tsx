a0d814e4a3fffe35ced7455aec78d5d2953371d9
Olena <nuchuporuk@gmail.com>
2020-02-26 19:53:56 +0100

    Fetch data from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import { Chart } from "react-google-charts";
 
@@ -7,6 +7,24 @@ const Wrapper = styled.div`
 `;
 
 const App: React.FC = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(
+        `https://api.airvisual.com/v2/city?city=Paris&state=Ile-de-France&country=France&key=${process.env.REACT_APP_AIR_VISUAL}`,
+        {
+          method: "GET",
+          redirect: "follow"
+        }
+      );
+
+      const resultText = await result.json();
+
+      console.log(resultText);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <Wrapper>
       <Chart
